e51952137e74f2e9ccaac841b11dc487ed52131e
bhargav27b <bhargav@sketchbrahma.com>
2022-06-28 22:24:39 +0530

    template literals

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
 "use strict"
 
-class User {
-    constructor(username, useremail, password) {
-        this.username = username;
-        this.useremail = useremail;
-        this.userpassword = password;
-    }
+let name = 'bhargav';
 
-    register() {
-        console.log(this.username + ' is now registered');
-    }
-}
-
-
-// inheriting User class 
-class Menber extends User {
-    constructor(username, useremail, password, member) {
-        super(username, useremail, password); // super() for using the parent class properties. Another way is to assign the parameters again (eg: this.username = username)
-        this.member = member;
-    }
-
-    getMembership() {
-        console.log(this.username + ' is subscribed to the ' + this.member + ' membership');
-    }
-}
-
-let bhargav = new Menber('bhargav', 'bhargavraju98', '123', 'prime');
-
-bhargav.register();
-bhargav.getMembership();
+console.log(`hello ${name}`);
