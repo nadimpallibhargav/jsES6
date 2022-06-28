1f0fd32fe9dd902b0979959cfad9c34f8bab69d4
bhargav27b <bhargav@sketchbrahma.com>
2022-06-28 18:57:22 +0530

    inheritance

--- main.js
+++ main.js
@@ -7,9 +7,24 @@ class User {
         this.userpassword = password;
     }
 
-    static countUsers() {
-        console.log('There are 50 users');
+    register() {
+        console.log(this.username + ' is now registered');
     }
 }
 
-User.countUsers();
+// inheriting User class 
+
+class Menber extends User {
+    constructor(username, useremail, password, member) {
+        super(username, useremail, password); // super() for using the parent class properties. Another way is to assign the parameters again (eg: this.username = username)
+        this.member = member;
+    }
+
+    getMembership() {
+        console.log(this.username + ' is subscribed to the ' + this.member + ' membership');
+    }
+}
+
+let bhargav = new Menber('bhargav', 'bhargavraju98', '123', 'prime');
+
+bhargav.getMembership();
