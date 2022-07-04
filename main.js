f29fbc5718ea6c9a707f1cca98e5914ff0732671
bhargav27b <bhargav@sketchbrahma.com>
2022-07-04 11:08:34 +0530

    number methods. isNaN()

--- main.js
+++ main.js
@@ -2,9 +2,5 @@
 
 ///////////// number methods /////////////
 
-// isFinite() method
-console.log(Number.isFinite(3));
-console.log(Number.isFinite(-3));
-console.log(Number.isFinite(0));
-console.log(Number.isFinite(Infinity));
-console.log(Number.isFinite(NaN));
+// isNaN() method. Returns true if the value is Not a Number(NaN)
+console.log(Number.isNaN(NaN));
