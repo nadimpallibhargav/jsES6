965e6cf7042923b64e2a19d36cb32371a7c8bf83
bhargav27b <bhargav@sketchbrahma.com>
2022-07-05 12:18:05 +0530

    Set() method in WeakMap example 2

--- main.js
+++ main.js
@@ -5,12 +5,20 @@ let carWeakMap = new WeakMap();
 let key1 = {
     id: 1
 }
-
 let car1 = {
     company: 'honda',
     modal: 'city'
 }
 
+let key2 = {
+    id: 2
+}
+let car2 = {
+    company: 'honda',
+    modal: 'civic'
+}
+
 carWeakMap.set(key1,car1);
+carWeakMap.set(key2,car2);
 
 console.log(carWeakMap);
