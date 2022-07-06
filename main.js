078fc6c1c31d5ec592481465c7c8cdc056823697
bhargav27b <bhargav@sketchbrahma.com>
2022-07-06 11:13:08 +0530

    Defining Promise() conditions

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 "use strict"
 
 let myPromise = new Promise((resolve, reject) => {
-    
+
+    let promiseStatus = "completed";
+
+    if(promiseStatus == "completed") {
+        resolve('SUCCESS');
+    }
+    else {
+        reject('FAILED');
+    }
+
 });
