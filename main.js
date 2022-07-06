ec959d3573e1cc212db8f9b1220b8ca2aee2a2db
bhargav27b <bhargav@sketchbrahma.com>
2022-07-06 12:54:11 +0530

    defining generator function

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 "use strict"
 
 function *generatorFunction() {
-    
+    console.log('hi');
+    yield 'step 1';
+    console.log('this');
+    yield 'step 2';
+    console.log('is');
+    yield 'step 3';
+    console.log('bhargav');
+    yield 'step 4';
 }
