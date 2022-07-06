8fe00fb6e01967d19361b60a62727422883bfa73
bhargav27b <bhargav@sketchbrahma.com>
2022-07-06 10:59:00 +0530

    Defining Promise() , it takes 1 parameter that is function with 2 values  "resolve" and "reject"

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict"
 
-// promises in js are similar to real life promises. 
-// If the promise was completed, then it is RESOLVED
-// If the promise was not done, then it is REJECTED
+let myPromise = new Promise((resolve, reject) => {
+    
+});
