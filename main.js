c2f7fc97bb4359f8995ae12ed5420e963a55cf0e
bhargav27b <bhargav@sketchbrahma.com>
2022-07-06 12:48:09 +0530

    Generators are functions that can be paused and then resumed as many times we want, At each pause it can yield/return a value. Generator functions name starts with *

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 "use strict"
 
-let myPromise = new Promise((resolve, reject) => {
-
-    let promiseStatus = "completed";
-
-    if(promiseStatus == "completed") {
-        resolve('SUCCESS');
-    }
-    else {
-        reject('FAILED');
-    }
-
-}); 
-
-myPromise.then((successMessage) => {
-    console.log(`the promise was ${successMessage}`);
-}).catch((failedMessage) => {
-    console.log(`the promise was ${failedMessage}`);
-})
+function *generatorFunction() {
+    
+}
