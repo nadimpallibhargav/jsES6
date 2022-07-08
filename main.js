e11b011c32c9a5e07da46bdd2a96ee3ade5cd2db
bhargav27b <bhargav@sketchbrahma.com>
2022-07-08 12:49:06 +0530

    reduce() method used for reducing the array and return single value, like adding all the values in array

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 "use strict"
 
-const companies = [
-    {
-        name: 'infosys',
-        start: 1990
-    },
-    {
-        name: 'tata',
-        start: 1970
-    },
-    {
-        name: 'mahindhra',
-        start: 2000
-    },
-]
+const ages = [23,28,3,21,45];
 
-const sortByYear = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
+const sumOfAges = ages.reduce((total, nextage) => (total + nextage), 0);
 
-console.log(sortByYear); 
+console.log(sumOfAges);
