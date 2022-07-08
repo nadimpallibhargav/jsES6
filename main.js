a56dd3c1358068acb9a636ca6f841822988e9be9
bhargav27b <bhargav@sketchbrahma.com>
2022-07-08 12:52:23 +0530

    reduce() example. can pass 2 parameters. first parameter for storing the addition of values and second parameter for the index of the next value

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 
 const ages = [23,28,3,21,45];
 
-const sumOfAges = ages.reduce((total, nextage) => (total + nextage), 0);
+const sumOfAges = ages.reduce((total, nextage) => (total + nextage));
 
 console.log(sumOfAges);
