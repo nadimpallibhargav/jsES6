6fa9a57db786352a319996a988881da66b9bbebd
bhargav27b <bhargav@sketchbrahma.com>
2022-07-08 15:57:45 +0530

    callback functions setTimeout() example

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 "use strict"
 
-const ages = [23,28,3,21,45];
+const blogPosts = [
+    {
+        title: 'first blog post',
+        content: 'content for the blog post one'
+    },
+    {
+        title: 'second blog post',
+        content: 'content for the blog post two'
+    },
+];
 
-let filterAges = ages.map(age => age * 2).filter(age => age >= 20).sort((a, b) => a - b ).reduce((a, b) => (a + b), 0);
+function blogRequest() {
+    setTimeout(() => {
+        let blogTitle = '';
+        blogPosts.forEach(singleBlogs => {
+            blogTitle += `<li>${singleBlogs.title}</li>`;
+        });
+        document.querySelector('body').innerHTML = blogTitle;
+    }, 3000);
+}
 
-console.log(filterAges);
+blogRequest();
