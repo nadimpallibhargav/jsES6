0f4b97c7a429d19d9e232bdf913af803f5796616
bhargav27b <bhargav@sketchbrahma.com>
2022-07-08 12:54:08 +0530

    reduce() example. can pass optional value in the reduce() for adding the extra value for the total

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 
 const ages = [23,28,3,21,45];
 
-const sumOfAges = ages.reduce((total, nextage) => (total + nextage));
+const sumOfAges = ages.reduce( (total, nextage) => (total + nextage), 10 );
 
 console.log(sumOfAges);
