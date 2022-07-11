78c37896426f9143dead8a248fda4fa7c515baa3
bhargav27b <bhargav@sketchbrahma.com>
2022-07-11 15:53:06 +0530

    AJAX example 2 with setInterval

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 "use strict"
 
-var xhttp = new XMLHttpRequest();                               /* creates an XMLHttpRequest object */
-xhttp.onreadystatechange = function() {                         /* 'onreadystatechange' property specifies a function to be executed every time the status of the XMLHttpRequest object changes */ 
-    if (this.readyState == 4 && this.status == 200) {           /* When readyState property is 4 and the status property is 200, the response is ready */
-       let serverResponse = xhttp.responseText;                 /* responseText property returns the server response as a text string */
-       console.log(serverResponse);
-    }
-};
-xhttp.open("GET", "http://localhost:5502/people.json", true);   /* GET request for the specific JSON file */
-xhttp.send();                                                   /* SEND request */
+function getUpdates() {
+    var xhttp = new XMLHttpRequest();                               
+    xhttp.onreadystatechange = function() {                         
+        if (this.readyState == 4 && this.status == 200) {           
+        let serverResponse = xhttp.responseText;                 
+        console.log(serverResponse);
+        }
+    };
+    xhttp.open("GET", "http://localhost:5502/people.json", true);   
+    xhttp.send();                                                   
+}
+
+setInterval(getUpdates, 1000);
