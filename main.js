1a9307e9afd8c7df68a88dbb055c8bb47f993e06
bhargav27b <bhargav@sketchbrahma.com>
2022-07-11 10:43:39 +0530

    async await example for fetching api

--- main.js
+++ main.js
@@ -1,47 +1,10 @@
 "use strict"
 
-const blogPosts = [
-    {
-        title: 'first blog post',
-        content: 'content for the blog post one'
-    },
-    {
-        title: 'second blog post',
-        content: 'content for the blog post two'
-    },
-];
+async function fetchData() {
+    const apiRes = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await apiRes.json();
 
-function blogRequest() {
-    setTimeout(() => {
-        let blogTitle = '';
-        blogPosts.forEach(singleBlogs => {
-            blogTitle += `<li>${singleBlogs.title}</li>`;
-        });
-        document.querySelector('body').innerHTML = blogTitle;
-    }, 1000);
+    console.log(data);
 }
 
-function addBlogPost(addPost) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            blogPosts.push(addPost);
-
-            const error = false;
-
-            if(error == true) {
-                reject('error has occured');
-            } else {
-                resolve();
-            }
-
-        }, 2000);
-    });
-}
-
-async function init() {
-    await addBlogPost({title: 'third blog post', content: 'content for the blog post three'});
-
-    blogRequest();
-}
-
-init();
+fetchData();
