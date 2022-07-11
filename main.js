74db80ea53848daa4b2389add0934b29b822a879
bhargav27b <bhargav@sketchbrahma.com>
2022-07-11 13:38:39 +0530

    http request for local JSON file

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 "use strict"
 
-let person = {
-    name: 'bhargav',
-    age: 23
-}
-
-person = JSON.stringify(person);
-
-console.log(person.name);
+var xhttp = new XMLHttpRequest();                               /* creates an XMLHttpRequest object */
+xhttp.onreadystatechange = function() {                         /* 'onreadystatechange' property specifies a function to be executed every time the status of the XMLHttpRequest object changes */ 
+    if (this.readyState == 4 && this.status == 200) {           /* When readyState property is 4 and the status property is 200, the response is ready */
+       let serverResponse = xhttp.responseText;                 /* responseText property returns the server response as a text string */
+       console.log(serverResponse);
+    }
+};
+xhttp.open("GET", "http://localhost:5502/people.json", true);   /* GET request for the specific JSON file */
+xhttp.send();                                                   /* SEND request */
