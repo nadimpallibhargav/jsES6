5f500b6888f408f2f7482f5fd0bbe84051dcae85
bhargav27b <bhargav@sketchbrahma.com>
2022-07-11 10:26:57 +0530

    promise.all() used instead using many .then()

--- main.js
+++ main.js
@@ -1,41 +1,9 @@
 "use strict"
 
-const blogPosts = [
-    {
-        title: 'first blog post',
-        content: 'content for the blog post one'
-    },
-    {
-        title: 'second blog post',
-        content: 'content for the blog post two'
-    },
-];
+const user = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
-function blogRequest() {
-    setTimeout(() => {
-        let blogTitle = '';
-        blogPosts.forEach(singleBlogs => {
-            blogTitle += `<li>${singleBlogs.title}</li>`;
-        });
-        document.querySelector('body').innerHTML = blogTitle;
-    }, 1000);
-}
+Promise.all([user]).then(values => 
 
-function addBlogPost(addPost) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            blogPosts.push(addPost);
-
-            const error = true;
-
-            if(error == true) {
-                reject('error hs occured');
-            } else {
-                resolve();
-            }
-
-        }, 2000);
-    });
-}
-
-addBlogPost({name: 'third blog post', content: 'content for the blog post three'}).then(blogRequest).catch(err => console.log(err));
+    console.log(values)
+    
+);
